28d11c7b36432106ba498dcfef7e8abc5c1c4d3a
Oleksii Turuta <alexey.turuta@gmail.com>
2017-07-08 11:34:48 +0300

    procesS

--- py/process1.py
+++ py/process1.py
@@ -8,23 +8,25 @@ import os
 import scipy.ndimage
 import matplotlib.pyplot as plt
 
-from skimage import measure, morphology
+##from mayavi import mlab
+from matplotlib import pyplot, cm
+
+#from skimage import measure, morphology
+import skimage
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Some constants
 #INPUT_FOLDER = '/home/oleksii/kaggle/Bowl/input/sample_images/'
-INPUT_FOLDER = '/home/oleksii/workspace/dataset/'
-
-#INPUT_FOLDER = '../../dataset/d1_SE000002/'
+INPUT_FOLDER = '../../dataset/d0_test'
+#INPUT_FOLDER = '../../dataset/d1_SE000002'
+#INPUT_FOLDER = '../../dataset/d2_SE000001'
 # INPUT_FOLDER = '/home/oleksii/kaggle/Bowl/trio/'
 # INPUT_FOLDER = '/home/oleksii/kaggle/Bowl/input3/'
-patients = os.listdir(INPUT_FOLDER)
-patients.sort()
 
 
 # Load the scans in given folder path
 def load_scan(path):
-    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices = [dicom.read_file(path + '/' + s, force=True) for s in os.listdir(path)]
     slices.sort(key=lambda x: int(x.InstanceNumber))
     try:
         slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
@@ -35,7 +37,6 @@ def load_scan(path):
         s.SliceThickness = slice_thickness
 
     return slices
-
 
 def get_pixels_hu(scans):
     image = np.stack([s.pixel_array for s in scans])
@@ -59,57 +60,117 @@ def get_pixels_hu(scans):
 
     return np.array(image, dtype=np.int16)
 
-print ("patients", patients)
-#['d1_SE000001', 'd1_SE000002'*, 'd1_SE000003', 'd2_SE000001'*, 'd2_SE000002', 'd3_Data']
-first_patient = load_scan(INPUT_FOLDER + patients[3])
-
+first_patient = load_scan(INPUT_FOLDER)
 first_patient_pixels = get_pixels_hu(first_patient)
-print(first_patient_pixels)
+
+
 plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
 plt.xlabel("Hounsfield Units (HU)")
 plt.ylabel("Frequency")
 plt.show()
 
+
 # Show some slice in the middle
-plt.imshow(first_patient_pixels[25], cmap=plt.cm.gray)
+##plt.imshow(first_patient_pixels[5], cmap=plt.cm.gray)
 plt.figure(1)
 plt.subplot(321)
-plt.imshow(first_patient_pixels[10], cmap=plt.cm.gray)
+plt.imshow(first_patient_pixels[5], cmap=plt.cm.gray)
 plt.subplot(322)
-plt.imshow(first_patient_pixels[20], cmap=plt.cm.gray)
+plt.imshow(first_patient_pixels[10], cmap=plt.cm.gray)
 
 plt.subplot(323)
-plt.imshow(first_patient_pixels[30], cmap=plt.cm.gray)
+plt.imshow(first_patient_pixels[15], cmap=plt.cm.gray)
 plt.subplot(324)
-plt.imshow(first_patient_pixels[40], cmap=plt.cm.gray)
+plt.imshow(first_patient_pixels[20], cmap=plt.cm.gray)
 
 plt.subplot(325)
-plt.imshow(first_patient_pixels[50], cmap=plt.cm.gray)
+plt.imshow(first_patient_pixels[25], cmap=plt.cm.gray)
 plt.subplot(326)
-plt.imshow(first_patient_pixels[35], cmap=plt.cm.gray)
+plt.imshow(first_patient_pixels[30], cmap=plt.cm.gray)
 
-plt.show()
+##plt.show()
+##print("SliceThickness", first_patient[0].SliceThickness)
+##print("PixelSpacing  ", first_patient[0].PixelSpacing)
 
 
 def resample(image, scan, new_spacing=[1, 1, 1]):
     # Determine current pixel spacing
+
     spacing = map(float, ([scan[0].SliceThickness] + scan[0].PixelSpacing))
+
     spacing = np.array(list(spacing))
 
-    resize_factor = spacing / new_spacing
-    new_real_shape = image.shape * resize_factor
-    new_shape = np.round(new_real_shape)
-    real_resize_factor = new_shape / image.shape
-    new_spacing = spacing / real_resize_factor
+    resize_factor 		= spacing / new_spacing
+    new_real_shape 		= image.shape * resize_factor
+    new_shape 			= np.round(new_real_shape)
+    real_resize_factor 	= new_shape / image.shape
+    new_spacing 		= spacing / real_resize_factor
 
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
 
     return image, new_spacing
 
+def noise_adding(image, psy):
+	img = np.array(image, copy=True)
+	noise = np.random.random(image.shape)
+	noise[noise >= 1-psy] = 1
+	noise[noise < 1-psy] = 0
+	std_let = image.std()
+	img = image + np.ndarray.round(noise*std_let, decimals=0)
 
-pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1, 1, 1])
-print("Shape before resampling\t", first_patient_pixels.shape)
-print("Shape after resampling\t", pix_resampled.shape)
+	return img
+
+def noise_reduction(image):
+	img = np.array(image, copy=True)  	
+#http://lagrange.univ-lyon1.fr/docs/scipy/0.17.1/ndimage.html
+	img = scipy.ndimage.filters.gaussian_filter(image, sigma=0.2, truncate=3.0)
+#	img = scipy.ndimage.fourier_gaussian(image, sigma=0.2)
+
+#	img = scipy.ndimage.filters.gaussian_gradient_magnitude(image, sigma=0.2, truncate=3.0)
+#	img = scipy.ndimage.filters.median_filter(image, 3)
+##	img = scipy.ndimage.filters.uniform_filter(image, size=3)
+
+	#size of image
+	#fix
+	return img
+
+def distance_calculation(image1, image2):
+#	scipy.ndimage.distance_transform_bf()
+	result = abs(image1 - image2)
+	unique, counts = np.unique(result, return_counts=True)
+	return counts[1:5].sum()
+
+####pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1, 1, 1])
+#print("Shape before resampling\t", first_patient_pixels.shape)
+#print("Shape after resampling\t", pix_resampled.shape)
+#print("spacing", spacing)
+
+
+image_with_noise = noise_adding(first_patient_pixels, 0.02)
+image_without_noise = noise_reduction(image_with_noise)
+
+how_much1 = distance_calculation(image_with_noise, image_without_noise)
+
+image_without_noise1 = noise_reduction(first_patient_pixels)
+how_much2 = distance_calculation(image_without_noise1, first_patient_pixels)
+
+how_much3 = distance_calculation(image_without_noise1, image_without_noise)
+
+image_without_noise2 = noise_reduction(image_without_noise1)
+how_much4 = distance_calculation(image_without_noise2, first_patient_pixels)
+how_much5 = distance_calculation(image_without_noise2, image_without_noise1)
+
+print ("how_much")
+print ("how_much with noise && reduction nose        :", how_much1)
+print ("how_much with reduction origin and origin    :", how_much2)
+print ("how_much reduction noise and reduction origin:", how_much3)
+
+print ("how_much 2 - origin                          :", how_much4)
+print ("how_much 2 - 1                               :", how_much5)
+
+
+exit();
+
 
 
 def plot_3d(image, threshold=-300):
@@ -118,7 +179,8 @@ def plot_3d(image, threshold=-300):
     p = image.transpose(2, 1, 0)
     p = p[:, :, ::-1]
 
-    verts, faces = measure.marching_cubes(p, threshold)
+#    verts, faces = measure.marching_cubes(p, threshold)
+    verts, faces = measure.marching_cubes_classic(p, threshold)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
@@ -137,9 +199,8 @@ def plot_3d(image, threshold=-300):
 
 
 plot_3d(pix_resampled, 400)
-
 print("exit")
-exit()
+
 
 
 def largest_label_volume(im, bg=-1):
@@ -193,11 +254,11 @@ def segment_lung_mask(image, fill_lung_structures=True):
     return binary_image
 
 
-segmented_lungs = segment_lung_mask(pix_resampled, False)
-segmented_lungs_fill = segment_lung_mask(pix_resampled, True)
+##segmented_lungs = segment_lung_mask(pix_resampled, False)
+##segmented_lungs_fill = segment_lung_mask(pix_resampled, True)
 
-plot_3d(segmented_lungs, 0)
+##plot_3d(segmented_lungs, 0)
 
-plot_3d(segmented_lungs_fill, 0)
+##plot_3d(segmented_lungs_fill, 0)
 
-plot_3d(segmented_lungs_fill - segmented_lungs, 0)
+##plot_3d(segmented_lungs_fill - segmented_lungs, 0)
